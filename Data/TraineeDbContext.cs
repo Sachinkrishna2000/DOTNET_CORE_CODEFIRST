a1bfcab0080ea2f83a0cc8eeb8b7529ee73dc88f
Sachinkrishna2000 <krishsachin2000@gmail.com>
2022-10-15 23:46:18 +0530

    Add project files.

---
+++ Data/TraineeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DOTNET_CORE_CODEFIRST.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DOTNET_CORE_CODEFIRST.Data
+{
+    public class TraineeDbContext : DbContext
+    {
+        public TraineeDbContext(DbContextOptions<TraineeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Trainee> Trainees { get; set; }
+
+    }
+}
